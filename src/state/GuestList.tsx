598e4df020c0609b5924decc3004774ec081d1ba
enoqueJonas <enoque.macanda@gmail.com>
2023-09-06 18:10:20 +0200

    ADD hdnleClick event on GuestList component

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState('');
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const handleClick = () => {
+    setGuests([...guests, name]);
+    setName('');
+  }
 
   return (
     <div>
       <input value={name} onChange={(e) => setName(e.target.value)} id="name"/>
-      <button>Add Guest</button>
+      <button onClick={handleClick}>Add Guest</button>
     </div>
   );
 }
